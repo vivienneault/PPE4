59b1041b4fab140412da74586b52a036741598d3
vivien <vivienneault@gmail.com>
2013-05-26 16:09:05 +0200

    V2

--- Json.php
+++ Json.php
@@ -4,8 +4,8 @@
 if(isset($_GET['getSocietes'])){
 	//Change la valeur de l'option de configuration "soap.wsdl_cache_enabled" et lui donne la valeur 0.
 	ini_set("soap.wsdl_cache_enabled","0");
-	// Déclaration et instanciation d'un nouveau SoapClient visant l'adresse "http://localhost:2773/WsGestionAffectations.asmx?WSDM"
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	// Déclaration et instanciation d'un nouveau SoapClient visant l'adresse "http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl"
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	//Appel du web Service "GetSocietes"
 	$rslt = $obj->GetSocietes();
 	//Récupère les valeurs renvoyées
@@ -21,7 +21,7 @@ if(isset($_GET['GetLesProjets'])){
 
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('codeSociete' => $_POST['cboSocietes']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetLesProjets($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetLesProjetsResult']->Projet;
@@ -34,7 +34,7 @@ if(isset($_GET['GetCollabProjet'])){
 
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('idProjet' => $_POST['cboProjets']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetCollabProjet($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetCollabProjetResult']->Collaborateur;
@@ -47,7 +47,7 @@ if(isset($_GET['GetCollabProjet'])){
 if(isset($_GET['getFamille'])){
 
 	ini_set("soap.wsdl_cache_enabled","0");
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetLesFamilles();
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetLesFamillesResult']->Famille;
@@ -60,7 +60,7 @@ if(isset($_GET['getMetier'])){
 
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('codeFamille' => $_POST['cboFamilles']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetLesMetiers($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetLesMetiersResult']->Metier;
@@ -73,7 +73,7 @@ if(isset($_GET['getCollabAjout'])){
 
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('codeMetier' => $_POST['cboMetiers']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetLesCollaborateurs($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetLesCollaborateursResult']->Collaborateur;
@@ -87,7 +87,7 @@ if(isset($_GET['CreerProjet'])){
 
 ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('unNom' => $_POST['nom'], 'unCodeSociete' => $_POST['societe']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->CreerProjet($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['CreerProjetResult'];
@@ -103,7 +103,7 @@ if (isset($_GET['InsererAffectation'])){
 	$dtpF = $dtpF[2].'-'.$dtpF[1].'-'.$dtpF[0];
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('matriculeCollaborateur' => $_POST['id'], 'idMetier' => $_POST['metier'], 'dateDebut' => $dtpD , 'dateFin' => $dtpF  , 'idProjet' => $_POST['idProjet']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->InsererAffectation($param);
 	header('Content-Type: application/json');
 }
@@ -112,7 +112,7 @@ if (isset($_GET['InsererAffectation'])){
 if(isset($_GET['GetAffectationsCollab'])){
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array('matriculeCollaborateur' => $_POST['id']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetAffectationsCollab($param);
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetAffectationsCollabResult']->Affectation;
@@ -124,7 +124,7 @@ if(isset($_GET['GetAffectationsCollab'])){
 //Test si l'url contient "GetCollaborateursAffectesProjet"
 if(isset($_GET['GetCollaborateursAffectesProjet'])){
 	ini_set("soap.wsdl_cache_enabled","0");
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetCollaborateursAffectesProjet();
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetCollaborateursAffectesProjetResult']->Collaborateur;
@@ -136,7 +136,7 @@ if(isset($_GET['GetCollaborateursAffectesProjet'])){
 //Test si l'url contient "GetChefsDeProjet"
 if(isset($_GET['GetChefsDeProjet'])){
 	ini_set("soap.wsdl_cache_enabled","0");
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->GetChefsDeProjet();
 	$rslt = (array)$rslt;
 	$tab = $rslt['GetChefsDeProjetResult']->Collaborateur;
@@ -149,7 +149,7 @@ if(isset($_GET['GetChefsDeProjet'])){
 if(isset($_GET['SupprimerAffectation'])){
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array(matriculeCollaborateur=>$_POST['matricule'],idProjet=>$_POST['idProjet'],rang=>$_POST['rang']);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->SupprimerAffectation($param);
 	header('Content-Type: application/json');
 }
@@ -160,7 +160,7 @@ if(isset($_GET['ModifierAffectation'])){
 	$dateF = substr($_POST['dateFin'],0,4).'-'.substr($_POST['dateFin'],4,2).'-'.substr($_POST['dateFin'],6,2);
 	ini_set("soap.wsdl_cache_enabled","0");
 	$param = array(matriculeCollaborateur=>$_POST['matricule'],idProjet=>$_POST['idProjet'],rang=>$_POST['rang'],dateDebutModif=>$dateD,dateFinModif=>$dateF);
-	$obj = new SoapClient("http://localhost:2773/WsGestionAffectations.asmx?WSDL");
+	$obj = new SoapClient("http://192.168.241.131/TestWs/WsGestionAffectations.asmx?wsdl");
 	$rslt = $obj->ModifierAffectation($param);
 	header('Content-Type: application/json');
 }
